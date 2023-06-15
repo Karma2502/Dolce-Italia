ad033799e45eba0c31f112e4cdf2864a4a3beb94
Karma2502 <ac4454458@gmail.com>
2023-06-15 08:12:45 -0500

    second - commit

---
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react' 
+import { View } from 'react-native' 
+import { Input, Icon, Button } from '@rneui/base' 
+import { Form, useFormik } from 'formik'; 
+import { initialValues } from './LoginForm.Data'
+import { validationSchema } from './LoginForm.Data'; 
+import { styles } from './LoginFORM.style'; 
+ 
+const LoginForm = () => { 
+ 
+  const [showPassword, setShowPassword] = useState(false); 
+ 
+  const formik = useFormik({ 
+      initialValues:initialValues(), 
+      validationSchema: validationSchema(), 
+      validateOnChange: false, 
+      onSubmit:(formValue)=>{ 
+        console.log("Formulario"); 
+        console.log(formValue); 
+      }, 
+  }); 
+ 
+  const showHiddenPassword=()=>{ 
+    setShowPassword(!showPassword); 
+  } 
+ 
+  const HiddenPassword=()=>{ 
+    setPassword(!Password); 
+  } 
+ 
+  return ( 
+    <View style={styles.content}> 
+       <Input 
+          placeholder='Correo Electronico' 
+          containerStyle={styles.input} 
+          rightIcon={ 
+            <Icon type="material-community" name="at" iconStyle={styles.icon} />
+          }  
+          onChangeText={(text)=>formik.setFieldValue("email" , text)} 
+          errorMessage={formik.errors.email} 
+        /> 
+        <Input 
+          placeholder='Contraseña' 
+          containerStyle={styles.input} 
+          secureTextEntry={showPassword ? false : true} 
+          rightIcon={ 
+            <Icon type='material-community' name={showPassword ? "eye-off-outline" : "eye-outline"} iconStyle={styles.icon} onPress={showHiddenPassword} /> 
+          }  
+          onChangeText={(text)=>formik.setFieldValue("password" , text)} 
+          errorMessage={formik.errors.password} 
+        /> 
+      <Button  
+        containerStyle={styles.btnContainer}  
+        buttonStyle={styles.btnLogin}  
+        size="md"   
+        onPress={formik.handleSubmit}  
+        title="Iniciar Sesión"/> 
+    </View> 
+  ) 
+} 
+ 
+export default LoginForm;
---
+++ src/screens/Account/UserLogeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserLogeScreen =() => {
+  return (
+    <View>
+      <Text>UserLogeScreen</Text>
+    </View>
+  );
+};
+export default UserLogeScreen;
